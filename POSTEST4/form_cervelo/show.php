742d0fce626e8b1e0787d5062316afa98f6536f5
Adhanslv <asyhab2002@gmail.com>
2022-10-09 18:55:36 +0800

    Postest 4

---
+++ POSTEST4/form_cervelo/show.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Pembelian</title>
+    <link rel="icon" href="https://www.freepnglogos.com/uploads/honda-logo-png/honda-motorcycles-logo-wing-10.png">
+</head>
+<body>
+    <h1>Data Pembelian</h1>
+    <?php if(isset($_POST['simpan'])){?>
+        <div class="form">
+       <table border="1">
+       <tr>
+            <td><h3>Nama</h3</td>
+            <td>
+                <h3 id="nama" class="jarak-atas"><?php echo $_POST['nama'] ?></h3>
+            </td>
+        </tr>
+        <tr>
+            <td><h3>Jenis Sepeda</h3></td>
+            <td>
+                <h3 id="jenis" class="jarak-atas"><?php echo $_POST['jenis'] ?></h3>
+            </td>
+        </tr>
+        <tr>
+            <td><h3>Harga Sepeda</h3></td>
+            <td>
+                <h3 id="harga" class="jarak-atas"><?php echo $_POST['harga'] ?></h3>
+            </td>
+        </tr>
+        <tr>
+            <td><h3>Tanggal Beli</h3></td>
+            <td>
+                <h3 id="tanggal" class="jarak-atas"><?php echo $_POST['tanggal'] ?></h3>
+            </td>
+        </tr>
+        <tr>
+            <td><h3>Jenis Warna</h3></td>
+            <td>
+                <h3 id="choose" class="jarak-atas"><?php echo $_POST['choose'] ?></h3>
+            </td>
+        </tr>
+       </table>
+       </div>
+    <?php } ?>
+</body>
+<style>
+
+h1{
+    margin-left:753px;
+}
+
+.form{
+ 	width: 1406px;
+ 	background-color: #fff;
+ 	position: fixed;
+ 	margin-left: 142px;
+	text-align: center;
+	top: 122px;
+	color: #25476a;
+}
+
+table{
+    border-collapse : collapse;
+    margin-left: 20px;
+    margin-top: 20px;
+    font-size: 14px;
+    width: 759px;
+    margin: 2% auto;
+}
+table td{
+    padding: 5px 5px;
+}
+
+</style>
+</html>
